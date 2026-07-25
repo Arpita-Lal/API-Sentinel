e43b75fbcbd0d7526e50e11130d09b8bf5efbd23
bitreaper42 <chaitanyacoder11@gmail.com>
2026-07-26 05:04:36 +0530

    feat(module1_kprobe): parse tcp_sendmsg arguments and log connection details

--- module1_kprobe/module1_kprobe-ebpf/src/main.rs
+++ module1_kprobe/module1_kprobe-ebpf/src/main.rs
@@ -1,8 +1,15 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{macros::kprobe, programs::ProbeContext};
+mod vmlinux;
+
+use aya_ebpf::{
+    macros::kprobe, 
+    programs::ProbeContext,
+    helpers::bpf_probe_read_kernel
+};
 use aya_log_ebpf::info;
+use crate::vmlinux::{sock, sock_common};
 
 #[kprobe]
 pub fn module1_kprobe(ctx: ProbeContext) -> u32 {
@@ -13,7 +20,31 @@ pub fn module1_kprobe(ctx: ProbeContext) -> u32 {
 }
 
 fn try_module1_kprobe(ctx: ProbeContext) -> Result<u32, u32> {
-    info!(&ctx, "kprobe called");
+    // int tcp_sendmsg(struct sock *sk, struct msghdr *msg, size_t size);
+    // read socket 
+    let sk: *const sock = ctx.arg(0).ok_or(0u32)?;
+    let common: sock_common =
+    unsafe { bpf_probe_read_kernel(&(*sk).__sk_common).map_err(|_| 0u32)? };
+    // Access connection data
+    let src_ip = unsafe {
+    common.__bindgen_anon_1.__bindgen_anon_1.skc_rcv_saddr
+    };
+
+    let dest_ip = unsafe {
+        common.__bindgen_anon_1.__bindgen_anon_1.skc_daddr
+    };
+
+    let dest_port = unsafe {
+        u16::from_be(common.__bindgen_anon_3.__bindgen_anon_1.skc_dport)
+    };
+
+    let src_port = unsafe {
+        common.__bindgen_anon_3.__bindgen_anon_1.skc_num
+    };
+    
+    let size: usize = ctx.arg(2).ok_or(0u32)?; 
+    // info!(&ctx, "kprobe called");
+    info!(&ctx, "tcp_sendmsg size: {} src_ip: {} src_port: {} dest_ip: {} dest_port: {}", size, src_ip, src_port, dest_ip, dest_port);
     Ok(0)
 }
 
